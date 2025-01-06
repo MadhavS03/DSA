21d6f623d0122dcda513681f17b352d332e1a1fc
MadhavS03 <madhav.sharma21@st.niituniversity.in>
2025-01-06 13:20:11 +0530

    more practice

---
+++ 07-Trees(BST)/constructor.py
@@ -0,0 +1,24 @@
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+# class BinarySearchTree:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.root = new_node
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+my_tree = BinarySearchTree()
+
+print(my_tree.root)
---
+++ 08-Hash Tables/constructor.py
@@ -0,0 +1,22 @@
+# We should always use prime number of addresses
+# increases randomness for how the key-value pairs are going to be distributed in the hash table
+# reduces the collisions
+
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
+
+        
+my_hash_table = HashTable()
+
+my_hash_table.print_table()
